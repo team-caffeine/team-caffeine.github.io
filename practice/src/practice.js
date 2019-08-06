19476c86d1c4ceba0c54a9ba8bef40bd49988c15
Alex Spencer <alexander.j.spencer@gmail.com>
2019-08-06 16:25:29 -0700

    working on practice js

--- practice/src/practice.js
+++ practice/src/practice.js
@@ -1,22 +1,49 @@
+import store from '../../data/store.js';
+
+// const ingredientButton = document.getElementsByClassName('ingredient-buttons');
+
+// let ingredient;
+
+// if(ingredient === 'espresso') {
+//     // console.log('true');
+// }
+// else {
+//     // console.log('false');
+// }
+
+// const result = detectIngredient(ingredientButton);
+
+// function detectIngredient(ingredientButton) {
+// }
+// }
+
+// for(let i = 0; i < ingredientButton.length; i++) {
+//     ingredientButton[i].addEventListener('click', function() {
+//         let ingredient = ingredientButton[i].id;
+//         // console.log(ingredient);
+//         console.log(ingredient);
+//         return ingredient;
+//     });
+// }
+
+let currentDrink = [];
+
 const ingredientButton = document.getElementsByClassName('ingredient-buttons');
-
-let ingredient = '';
-
-
-if(ingredient === 'espresso') {
-    console.log('true');
+for(let i = 0; i < ingredientButton.length; i++) {
+    const clickedIngredient = ingredientButton[i];
+    clickedIngredient.addEventListener('click', getIngredient);
 }
-else {
-    console.log('false');
+function getIngredient() {
+
+    let ingredient = event.currentTarget;
+    ingredient.id;
+    // console.log(ingredient.id);
+    store.saveResults(ingredient.id);
 }
 
-detectIngredient(ingredientButton);
+let ingredient = store.getIngredient();
+console.log(ingredient);
 
-function detectIngredient(ingredientButton) {
-    for(let i = 0; i < ingredientButton.length; i++) {
-        ingredientButton[i].addEventListener('click', function() {
-            ingredient = ingredientButton[i].id;
-        });
-    }
-    return ingredient;
-}
+
+
+    
