b0fd0e4a5958334826379c30547b9b63efc9aa93
Alex Spencer <alexander.j.spencer@gmail.com>
2019-08-08 13:08:09 -0700

    built store methods

--- data/store.js
+++ data/store.js
@@ -80,7 +80,18 @@ const store = {
             finishedDrinks.push(madeDrink);
         }
         store.save('finished-drinks', finishedDrinks);
-    }
+    },
+    getRandomDrink() {
+        let randomDrinkList = store.get('random-drinks');
+        if(!randomDrinkList) {
+            randomDrinkList = [];
+        }
+        return randomDrinkList;
+    },
+    saveRandomDrinkList(randomDrinks) {
+        const json = JSON.stringify(randomDrinks);
+        store.storage.setItem('random-drinks', json);
+    },
 };
 
 const sessionStore = {
